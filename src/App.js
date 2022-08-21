81b3171862d84704b3f2c48c0a7e7f46cbf4aa84
Lavdeep Singh <44914568+coolguy08@users.noreply.github.com>
2022-08-21 20:15:43 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
       </div>
     }
 
-     async function convert(){
+     async function convert=()=>{
        setconverting(true);
        console.log(from,to);
          const res=await fetch(base+`getrates?from=${from}&to=${to}`);
